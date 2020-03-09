aa3cb1b19cf81db7dc2816bc36d6cf097d02a327
rijkvanzanten <rijkvanzanten@me.com>
2020-03-09 11:36:10 -0400

    Tweak code style to match project

--- src/SDK.ts
+++ src/SDK.ts
@@ -772,7 +772,7 @@ export class SDK {
    * Limit is hardcode in -1 because is necessary get all settings rows.
    */
   public getSettings(params: QueryParamsType = {}) {
-    params.limit=-1;
+    params.limit = -1;
     return this.api.get<ISettingsResponse>("/settings", params);
   }
 
